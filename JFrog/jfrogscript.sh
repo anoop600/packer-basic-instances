8888fb30285d2bdb382bbf5e25e8a4a429fc0e41
root <root@ip-172-31-11-212.ap-southeast-1.compute.internal>
2019-04-11 06:30:11 +0000

    java-mysql

--- JFrog/jfrogscript.sh
+++ JFrog/jfrogscript.sh
@@ -1,5 +1,7 @@
 sudo apt-get update && sudo apt-get install unzip
 mkdir /opt/jfrog
 cd /opt/jfrog
-curl -LO https://api.bintray.com/content/jfrog/artifactory/jfrog-artifactory-oss-$latest.zip;bt_package=jfrog-artifactory-oss-zip
-
+wget https://s3-ap-southeast-1.amazonaws.com/jfrog-zip/jfrog-artifactory-oss-6.9.1.zip
+unzip jfrog-artifactory-oss-6.9.1.zip
+cd artifactory-oss-6.9.1/bin/
+nohup sh artifactory.sh &
---
+++ java-mysql/java-mysqlscript.sh
@@ -0,0 +1,8 @@
+sudo apt update -y
+sudo apt install openjdk-8-jdk -y
+
+echo "JAVA is Installed"
+
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password minduser'
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password minduser'
+sudo apt-get -y install mysql-server
